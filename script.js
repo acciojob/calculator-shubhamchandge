b5ebed75d274952f72ad0e6bd291cdb6bfe2df8c
shubham chandge <115560962+shubhamchandge@users.noreply.github.com>
2023-01-27 12:43:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your code here
+let output = document.querySelector('.output')
+let string = '';
+let buttons = document.querySelectorAll('.input-button');
+
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', (e) => {
+        try {
+            if (e.target.innerHTML == '=') {
+                string = eval(string);
+                output.value = string;
+            }
+            else if (e.target.innerHTML == "RESET") {
+                string = ""
+                output.value = string;
+            }
+            else {
+                string = string + e.target.innerHTML;
+        //console.log(string);
+                output.value = string;
+            }
+        }
+        catch (err) {
+            output.value = " Syntax Error";
+            string = '';
+        }
+    })
+})
